051f81a689926e4ea650fd774839d79dd5e91afb
Jonatan Arrocha Kang <erickangweisz@gmail.com>
2019-12-26 11:00:05 +0000

    generate token service and implement it like interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { TokenService } from './interceptors/token.service';
 
 @NgModule({
   declarations: [
@@ -13,7 +14,13 @@ import { AppComponent } from './app.component';
     BrowserModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: TokenService,
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -14,13 +14,13 @@ export class UsersService {
     let params = new HttpParams().append('page', '2');
     params = params.append('name', 'Jonatan Arrocha');
 
-    const headers = new HttpHeaders({
+    /*const headers = new HttpHeaders({
       'token-user': 'AAAABBBBCCCC12345678'
-    });
+    });*/
 
-    return this.http.get('https://reqress.in/api/user', {
+    return this.http.get('https://reqres.in/api/user', {
       params,
-      headers
+      //headers
     }).pipe(
       map(res => {
         return res['data'];
